b64c0a2ff1e55226686f9827c11cb84854620db0
Philip Fowler <philipwfowler@gmail.com>
2020-09-28 15:41:50 +0100

    L2/3 ok

---
+++ bin/qb-modify-latex.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+
+import argparse
+
+import gumpy
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--latex_file",required=True,help="the path to a LaTeX file produced by nbconvert")
+    options = parser.parse_args()
+
+    keep_line =True
+
+    with open(options.latex_file,'r') as INPUT:
+        for line in INPUT:
+            if "begin{tcolorbox}" in line:
+                keep_line=False
+
+            if "section{" in line:
+                line=line.replace('section{','section*{')
+
+            if "begin{document}" in line:
+                line+="\n\date{}"
+
+            if "0.9\\linewidth" in line:
+                line=line.replace("0.9\\linewidth","0.75\\linewidth")
+
+            if "0.9\\paperheight" in line:
+                line=line.replace("0.9\\paperheight","0.5\\paperheight")
+
+            if keep_line:
+                print(line.rstrip())
+            elif "end{tcolorbox}" in line:
+                keep_line=True
