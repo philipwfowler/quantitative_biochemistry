1f4f8d66d1e6f4c45293be6e5b08cbbf568c3566
Philip W Fowler <philipwfowler@users.noreply.github.com>
2021-10-17 15:40:32 +0100

    added compile.sh

---
+++ quantitative_biochemistry/compile.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+for i in `ls Lecture-*.ipynb`; do
+    filename=${i%.*}
+    jupyter-nbconvert-3.8 --to latex ${filename}.ipynb
+    mv ${filename}.tex foo.tex
+    qb-modify-latex.py --latex foo.tex > ${filename}.tex
+    pdflatex ${filename}.tex
+done;
+    
+rm foo.tex
+rm Lecture*.out
+rm Lecture*.aux
+rm Lecture*.log
+
