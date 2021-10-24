c851dd0d5d4b82c3f379678dce29d9301c570ff5
Philip W Fowler <philipwfowler@users.noreply.github.com>
2021-10-24 18:53:13 +0100

    edited L3,4

---
+++ quantitative_biochemistry/compile-specific.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+i=$1
+#for i in `ls Lecture-$0*.ipynb`; do
+filename=${i%.*}
+echo $filename
+jupyter-nbconvert-3.8 --to latex ${filename}.ipynb
+mv ${filename}.tex foo.tex
+qb-modify-latex.py --latex foo.tex > ${filename}.tex
+pdflatex ${filename}.tex
+#done;
+    
+rm foo.tex
+rm Lecture*.out
+rm Lecture*.aux
+rm Lecture*.log
+
